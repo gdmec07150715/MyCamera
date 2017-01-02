46f43b72debfe59c21a28c334eda42bb9193bbf9
gdmec07150715 <1034232757@qq.com>
2017-01-02 10:49:36 +0800

    Initial commit

---
+++ app/src/main/java/com/cn/sgdmec07150715/mycamera/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cn.sgdmec07150715.mycamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chengd.mycamera.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
